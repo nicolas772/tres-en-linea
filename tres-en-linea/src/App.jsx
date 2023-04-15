d44c5e4e77b6d7b38076707f284496c62ff5ac78
Nicolas Araya Urrutia <nicolas.arayau@sansano.usm.cl>
2023-04-15 14:33:59 -0400

    manejo de local storage para boton de reset juego

--- tres-en-linea/src/App.jsx
+++ tres-en-linea/src/App.jsx
@@ -36,6 +36,9 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
   }
 
   const checkEndGame = (newBoard) => { //si ningun cuadrado es null, devuelve true
